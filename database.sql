5b5a804260cb55abfdc7a5c406cf701fc8fa983d
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-22 19:59:10 -0500

    redo heroku db

--- database.sql
+++ database.sql
@@ -3,8 +3,7 @@ CREATE DATABASE discordapi;
 CREATE TABLE discordquestions(
     question_id SERIAL PRIMARY KEY,
     question VARCHAR(255),
-    answer VARCHAR(255),
-    answer_choices text[]
+    answer VARCHAR(255)
 );
 
--- INSERT INTO discordquestions ( question, answer, answer_choices) VALUES ( 'How do you connect your github repository locally?', 'git clone [quick setup SSH]', ARRAY ['git copy [quick setup SSH]','git clone [quick setup SSH]']);
+-- INSERT INTO discordquestions ( question, answer) VALUES ( 'To connect your github repository locally, you would use "git clone [SSH KEY]"', 'true');
