330a16b165c738bf70803af75bd855ea52a41ae6
General Assembly <generalassembly@tests-MacBook-Air.local>
2023-01-23 09:02:02 -0500

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const cors = require('cors')
-// const pool = require("./db.js")
 const pool = require('./postgres')
 
 app.use(express.json())
